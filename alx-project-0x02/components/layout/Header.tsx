52428715cc91cce7c6c8d27cead37bb6edc23000
Opeyemi <Opeyemi5055@yahoo.com>
2025-07-22 23:15:45 +0100

    ass2

--- alx-project-0x02/components/layout/Header.tsx
+++ alx-project-0x02/components/layout/Header.tsx
@@ -1,1 +1,18 @@
-console.log("hello");
+import React from "react";
+import Link from "next/link";
+
+const Header: React.FC = () => {
+  return (
+    <header className="  bg-gray-500">
+      <div className="flex justify-between items-center p-4 w-full text-white">
+        <h1 className="text-4xl font-bold">ALX</h1>
+        <nav className="flex space-x-6 font-semibold">
+          <Link href="/home">Home</Link>
+          <Link href="/about">About</Link>
+        </nav>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
--- alx-project-0x02/pages/index.tsx
+++ alx-project-0x02/pages/index.tsx
@@ -1,9 +1,15 @@
 import React from "react";
+import Header from "@/components/layout/Header";
 
 const Home: React.FC = () => {
   return (
-    <div className="flex justify-center items-center">
-      <h1 className="text-center text-4xl">Hello World</h1>
+    <div className="">
+      <Header />
+      <main className="flex items-center justify-center min-h-screen">
+        <h1 className="text-center text-4xl text-blue-500 font-bold">
+          My Dashboard
+        </h1>
+      </main>
     </div>
   );
 };
