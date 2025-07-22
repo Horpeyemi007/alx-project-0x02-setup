5753a02bbe507daac207fd7df3a0878d881ca721
Opeyemi <Opeyemi5055@yahoo.com>
2025-07-22 22:27:45 +0100

    first commit

---
+++ pages/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Home: React.FC = () => {
+  return (
+    <div className="flex justify-center items-center">
+      <h1 className="text-center text-4xl">Hello World</h1>
+    </div>
+  );
+};
+
+export default Home;
